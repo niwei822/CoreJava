dd039b68e78bdd63af9f59fbcdb1cc8e9a03e484
niwei822 <cecily.li.822@gmail.com>
2023-09-08 16:10:07 -0700

    add pyramid problem

---
+++ src/JavaBasic/Pyramid.java
@@ -0,0 +1,19 @@
+package JavaBasic;
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int k=1;
+		for(int i=0;i<4;i++) {
+			for(int j=0;j<i+1;j++) {
+				System.out.print( k);
+				System.out.print(" ");
+				k++;
+				
+			}
+			System.out.println("");
+		}
+
+	}
+}
