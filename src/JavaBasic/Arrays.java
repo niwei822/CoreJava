341ac0f0dc31faf045a02789a8a04edbb7cf72f2
niwei822 <cecily.li.822@gmail.com>
2023-09-09 16:34:32 -0700

    add array practices

---
+++ src/JavaBasic/Arrays.java
@@ -0,0 +1,34 @@
+package JavaBasic;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = new int[5];
+		int b[] = {1,2,3,4,5};
+	
+		for(int i=0;i<a.length;i++) {
+			a[i] = i;
+			//System.out.print(a[i]);
+		}
+		int c[][] = new int[2][3];
+		c[0][0]=3;
+		c[0][1]=4;
+		c[0][2]=19;
+		c[1][0]=2;
+		c[1][1]=7;
+		c[1][2]=10;
+		int min = c[0][0];
+		for(int i=0;i<2;i++) {
+			for(int j=0;j<3;j++) {
+				if(c[i][j]<min) {
+					min=c[i][j];
+				}
+				
+			}
+		}
+		System.out.println(min);
+		
+	}
+
+}
---
+++ src/JavaBasic/SwapNumbers.java
@@ -0,0 +1,26 @@
+package JavaBasic;
+
+public class SwapNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=6;
+		int temp;
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(a);
+		System.out.println(b);
+		
+		int c=17;
+		int d=8;
+		c=c+d;
+		d=c-d;
+		c=c-d;
+		System.out.println(c);
+		System.out.println(d);
+
+	}
+
+}
