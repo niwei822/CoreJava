e892a6e5119005d6af6a981e6d2712b2fb8739eb
niwei <69645683+niwei822@users.noreply.github.com>
2023-11-26 20:26:19 -0800

    Time: 2 ms (68.19%), Space: 45.6 MB (5.32%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,12 +3,11 @@ class Solution {
         int count = 0;
         for(int i=0;i<nums.length;i++) {
             if(nums[i] != 0) {
+                int temp = nums[count];
                 nums[count] = nums[i];
+                nums[i] = temp;
                 count++;
             }
         }
-        for(int i=count;i<nums.length;i++) {
-            nums[i] = 0;
-            }
     }
 }
